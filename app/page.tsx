440204704e2b1af25cf802ac5ee0a12ffdb054ee
gianttelepathicsquid <kevinxrp@gmail.com>
2024-10-31 14:55:47 -0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import WarehouseAutomationGuide from '@/components/WarehouseAutomationGuide'
+
+export default function Home() {
+  return (
+    <main>
+      <WarehouseAutomationGuide />
+    </main>
+  )
+}
